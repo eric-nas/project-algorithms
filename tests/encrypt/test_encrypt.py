b1ac6532e5592ee2d01052b44a16392794e133c4
eric-nas <ericnascimentocruzc@gmail.com>
2024-02-01 19:08:11 -0300

    Req2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('Eric', 2) == 'ci_rE'
+    assert encrypt_message('Eric', 3) == 'irE_c'
+    assert encrypt_message('Eric', 999) == 'cirE'
+    with pytest.raises(TypeError):
+        encrypt_message("string", "teste")
+    with pytest.raises(TypeError):
+        encrypt_message(123, 1)
