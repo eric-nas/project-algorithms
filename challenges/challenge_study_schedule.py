ddde2b715eda164a736862ab8f172c1c728927a6
eric-nas <ericnascimentocruzc@gmail.com>
2024-02-01 17:19:11 -0300

    Req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    saída = 0
+    for time in permanence_period:
+        if (
+            time[0] is None
+            or time[1] is None
+            or isinstance(time[0], str)
+            or isinstance(time[1], str)
+        ):
+            return None
+        if time[0] <= target_time and time[1] >= target_time:
+            saída += 1
+    return saída
