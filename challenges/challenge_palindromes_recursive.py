0c392587929fb5982304cbd7179e60d946eb5334
eric-nas <ericnascimentocruzc@gmail.com>
2024-02-02 11:41:16 -0300

    Req3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == '':
+        return False
+    if len(word) == 1:
+        return True
+    letter1 = word[high_index]
+    letter2 = word[low_index]
+    if letter1 == letter2 and word[1:-1] == '':
+        return True
+    if letter1 == letter2:
+        return is_palindrome_recursive(word[1:-1], 0, -1)
+    return False
